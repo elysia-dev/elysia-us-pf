d84ff8b78d196bbdc1f087a93d0909f259491628
qpzm <lhm1442@gmail.com>
2022-07-11 15:15:21 +0900

    [EUP-18] Delete console import

--- packages/hardhat/contracts/NftBond.sol
+++ packages/hardhat/contracts/NftBond.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
 
 error AlreadyInitialized();
 error InitProject_SenderNotAuthorized();
