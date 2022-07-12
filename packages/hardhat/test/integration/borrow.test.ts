5bbf7d092ce8b2c4ff3fde09cc16e116ad9028a5
Jeewoo Lee <44099418+jeewoo-lee@users.noreply.github.com>
2022-07-12 12:36:13 +0900

    it transfers usdc to the user

--- packages/hardhat/test/integration/borrow.test.ts
+++ packages/hardhat/test/integration/borrow.test.ts
@@ -1,6 +1,8 @@
 import { expect } from "chai";
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
-import { initProject } from "../utils/controller";
+import { initProject, initProjectInput } from "../utils/controller";
+import { advanceTimeTo } from "../utils/time";
 import { VALID_PROJECT_ID } from "./../utils/constants";
 
 const finalAmount = ethers.utils.parseUnits("2000", 6);
@@ -14,30 +16,34 @@ export function borrowTest(): void {
       await this.contracts.usdc
         .connect(this.accounts.deployer)
         .approve(this.contracts.controller.address, finalAmount);
+      await advanceTimeTo(initProjectInput.depositEndTs);
     });
 
     // Q. Is it borrow test?
     describe("success", async function () {
-      it("should transfer usdc", async function () {
-        const beforeBalance = await this.contracts.usdc.balanceOf(
-          this.contracts.controller.address
+      it.only("should transfer usdc", async function () {
+        const theProject = await this.contracts.controller.projects(projectId);
+        const amount = theProject.currentAmount;
+        
+        const userBalance = await this.contracts.usdc.balanceOf(
+          this.accounts.deployer.address
         );
 
         const tx = await this.contracts.controller
           .connect(this.accounts.deployer)
-          .repay(projectId, finalAmount);
+          .borrow(projectId);
 
         expect(
-          await this.contracts.usdc.balanceOf(this.contracts.controller.address)
-        ).to.equal(beforeBalance.add(finalAmount));
+          await this.contracts.usdc.balanceOf(this.accounts.deployer.address)
+        ).to.equal(userBalance.add(amount));
 
-        expect(tx)
-          .to.emit(this.contracts.usdc, "Transfer")
-          .withArgs(
-            this.accounts.deployer,
-            this.contracts.controller,
-            finalAmount
-          );
+        // expect(tx)
+        //   .to.emit(this.contracts.usdc, "Transfer")
+        //   .withArgs(
+        //     this.accounts.deployer,
+        //     this.contracts.controller,
+        //     finalAmount
+        //   );
       });
     });
   });
